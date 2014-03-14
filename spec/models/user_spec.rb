c0c66041661c30caf0217141f99e8d77e2c3f757
patorash <chariderpato@gmail.com>
2014-03-15 01:50:22 +0900

    Userのテストを書いた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,5 +21,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:role) { FactoryGirl.create(:role) }
+  context '正しいデータを入れた場合' do
+    subject { FactoryGirl.build(:user, role: role) }
+    its(:email) { should == 'admin@email.com' }
+    its(:password) { should == 'password' }
+    it { should be_valid }
+    its(:save) { should be_true }
+  end
+  context '必須項目チェック' do
+    subject {User.new}
+    it '必須項目が空欄だとエラーになること' do
+      subject.should_not be_valid
+      subject.should have(2).errors_on(:email)
+      subject.should have(3).errors_on(:password)
+      subject.should have(1).errors_on(:role)
+    end
+  end
 end
